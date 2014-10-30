8e58ac959702e6045426eb3de7c403e19b8252e8
Fernando Bueno <buenofernando13@gmail.com>
2014-10-30 08:49:01 -0200

    Create Redirecionar.js

---
+++ Redirecionar.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+   var isMobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+   if (isMobile)
+   {
+      location.replace("../mobile.php");
+   }  
+</script>
